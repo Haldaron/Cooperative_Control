06a58629d9a49d8287ef4742b272207ea22db049
Daniel Ochoa <deot95@gmail.com>
2014-10-21 20:01:26 -0500

    Ajustes Varios Antes de Buscar Implementacción de grafo con pesos. Nota: Se deja en funcionamiento hasta la inicialización de posibles caminos para cada uno de los carros.

--- CooperativeControl/src/mundo/Grafo.java
+++ CooperativeControl/src/mundo/Grafo.java
@@ -3,60 +3,63 @@ package mundo;
 import java.util.ArrayList;
 
 public class Grafo {
-	
+
 	public int V;
 	public int E;
 	public ArrayList<Integer>[] adjuntos;  
 	private Nodo[][] malla;
-	
-	
+
+
 	/**
 	 * Crea un grafo de listas de adyacencia a partir de una matriz cuadrada que entra por parámetro.
 	 * @param malla
 	 */
-	
+
 	public Grafo (Nodo[][] grid){
-		
+
 		malla=grid;
 		int N=malla[0].length;
 		V=(int)Math.pow(N, 2);
 		adjuntos= new ArrayList[V];
-		
+
 		int i,j,k,l,m;
 		Nodo n;
-		  for (k = 0; k < V; k++) {
-	            adjuntos[k] = new ArrayList<Integer>();
-	            j=k%N;
-	            i=(k-j)/N;
-	            for(l=i-1;l<=i+1;l++){
-	            	for(m=j-1;m<=j+1;m++){
-	            		if(m!=j || l!=i){
-	            			try{
-	            				n=malla[l][m];
-	            				adjuntos[k].add(l*N+m);
-	            			}catch(IndexOutOfBoundsException e){
-	            				
-	            			}
-	            		}
-	            	}
-	            }
-	            
-	            
-	            
-	        }
+		for (k = 0; k < V; k++) 
+		{
+			adjuntos[k] = new ArrayList<Integer>();
+			j=k%N;
+			i=(k-j)/N;
+			for(l=i-1;l<=i+1;l++)
+			{
+				for(m=j-1;m<=j+1;m++)
+				{
+					if(m!=j || l!=i)
+					{
+						try
+						{
+							n=malla[l][m];
+							adjuntos[k].add(l*N+m);
+						}
+						catch(IndexOutOfBoundsException e)
+						{
+
+						}
+					}
+				}
+			}
+		}
 		
-	
-			
+
 	}
 
 
-	
+
 	public ArrayList<Integer> adj(int v){
 		if(v<0||v>=V) throw new IndexOutOfBoundsException();
 		return adjuntos[v];
-		
+
 	}
-	
+
 	public int getV() {
 		return V;
 	}
@@ -85,8 +88,8 @@ public class Grafo {
 	public void setAdjuntos(ArrayList<Integer>[] adjuntos) {
 		this.adjuntos = adjuntos;
 	}
-	
-	
-	
+
+
+
 
 }
